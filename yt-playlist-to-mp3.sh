814caa19093410ac14fa061b7c68f9105704b363
ebiwoxite <77335575+ebiwoxite@users.noreply.github.com>
2023-08-19 19:34:30 +0200

    Update yt-playlist-to-mp3.sh
    added comments
    removed illegal characters for windows file names
    cleaned the name of the file
    now gets the artist from yt-dlp instead of the uploader
    fixed a mistake that caused error messages in the mv command
    & other improvements

--- yt-playlist-to-mp3.sh
+++ yt-playlist-to-mp3.sh
@@ -5,32 +5,41 @@ cd $directory
 rm *
 echo "removed all files in $directory"
 
-
 playlist=$1
-yt-dlp -f 'ba' -x --audio-format mp3 $1 -o '%(uploader)s - %(title)s.mp3'
-echo "finished downloading"
+yt-dlp -f 'ba' -x --audio-format mp3 $1 -o '%(artist)s - %(title)s.mp3'
+
 for file in *; do
-    #renaming files without the Topic part 
-    newfile=$(echo "$file" | sed 's/ - .* - / - /')
-    mv "$file" "$newfile"
-    echo "Renamed $newfile"
+    #cleaning the name and applying tags (artist/title) to the file
+
+    #removing the NA part (when there is no artist defined) 
+    newfile=$(echo "$file" | sed 's/NA - //')
     
-    #applying tags (artist/title) to the file
-    filename=$(basename -s ".mp3" "$newfile")
-    echo "Applying tags..." 
+    #removes any part beween () or []
+    newfile=$(echo "$newfile" | sed 's/ *(.*)//') 
+    newfile=$(echo "$newfile" | sed 's/\[.*\]//')
+  
+    basename="${newfile%%.mp3*}" #remove the .mp3
+    basename="${basename//\'/\'\\\'\'}"
+
     delimiter=' - '
-    artist="${filename%$delimiter*}"
-    artist="${artist//\'/\'\\\'\'}"
-    title="${filename#*$delimiter}"
-    title="${title//\'/\'\\\'\'}"
+    artist="${basename%$delimiter*}"
+    artist="${artist%%,*}" #if there are featurings, it only take the first artist
+    
+    title="${basename#*$delimiter}"
 
     if [ -n "$artist" ] && [ -n "$title" ]; then #to verify if they are null
-      kid3-cli -c "set artist '$artist'" -c "set title '$title'" "$newfile"
+        echo "Applying tags..." 
+        kid3-cli -c "set artist '$artist'" -c "set title '$title'" "$file"
     else
-      echo "problem while applying tags $filename to $newfile"
+        echo "problem while applying tags $file"
     fi
     
+    #removes any illegal characters for file names 
+    newfile=$(echo "$newfile" | tr -d ':?|!$#%&{}<>*/')   
+    mv "$file" "$newfile"
+    echo "Renamed $newfile"
     # [COMING NEXT] putting the thumbnail of the video
 done
 echo "Finished renaming and tagging files"
+echo "You can find all your awesome music in $directory"
 echo "You can find all your awesome music in $directory"
