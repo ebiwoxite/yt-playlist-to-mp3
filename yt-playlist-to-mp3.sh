aff5fa55f037039240a92f6004cc87dedb727a28
ebiwoxite <77335575+ebiwoxite@users.noreply.github.com>
2023-09-12 22:40:01 +0200

    Update yt-playlist-to-mp3.sh
    improved the way i escaped quotes while applying tags
    improved the identifying of title and artist
    now it takes the uploader as artist if there is no artist defined

--- yt-playlist-to-mp3.sh
+++ yt-playlist-to-mp3.sh
@@ -4,32 +4,46 @@ directory="/youneedtochangethis"
 cd $directory
 rm *
 echo "removed all files in $directory"
-
 playlist=$1
-yt-dlp -f 'ba' -x --audio-format mp3 $1 -o '%(artist)s - %(title)s.mp3'
+yt-dlp -f 'ba' -x --audio-format mp3 $1 -o '%(uploader)sµ%(artist)sµ%(title)s.mp3'
 
 for file in *; do
-    #cleaning the name and applying tags (artist/title) to the file
-
-    #removing the NA part (when there is no artist defined) 
-    newfile=$(echo "$file" | sed 's/NA - //')
-    
+    #cleaning the name and applying tags (artist/title) to the file 
+    newfile=$(echo "$file" | sed 's/ - Topic//')
+ 
     #removes any part beween () or []
     newfile=$(echo "$newfile" | sed 's/ *(.*)//') 
     newfile=$(echo "$newfile" | sed 's/\[.*\]//')
-  
+
     basename="${newfile%%.mp3*}" #remove the .mp3
-    basename="${basename//\'/\'\\\'\'}"
+    basename="${basename// - /µ}"
+    IFS='µ'
+    read -r uploader artist title <<< "$basename"
 
-    delimiter=' - '
-    artist="${basename%$delimiter*}"
-    artist="${artist%%,*}" #if there are featurings, it only take the first artist
+    if [[ $title == *"µ"* ]]; then
+        echo "title contains artist"
+        read -r artist title <<< "$title"
+    fi
+    IFS=' '
     
-    title="${basename#*$delimiter}"
+    if [[ $artist == "NA" ]]; then
+        echo 'artist is NA' 
+        artist=$uploader
+        artist="${artist%% Officiel*}"	
+        artist="${artist%% Official*}"
+        artist="${artist%%VEVO*}"
+    fi
+    #if there are featurings, it only take the first artist
+    artist="${artist%%,*}" 
+    artist="${artist%%feat*}"
 
+    #strip spaces from the end of the strings
+    artist=$(echo "$artist" | sed 's/[[:space:]]*$//')
+    title=$(echo "$title" | sed 's/[[:space:]]*$//')
     if [ -n "$artist" ] && [ -n "$title" ]; then #to verify if they are null
         echo "Applying tags..." 
-        kid3-cli -c "set artist '$artist'" -c "set title '$title'" "$file"
+        kid3-cli -c "set artist \"$artist\"" -c "set title \"$title\"" "$file"
+        newfile="$artist - $title.mp3"
     else
         echo "problem while applying tags $file"
     fi
@@ -40,5 +54,6 @@ for file in *; do
     echo "Renamed $newfile"
     # [COMING NEXT] putting the thumbnail of the video
 done
+
 echo "Finished renaming and tagging files"
 echo "You can find all your awesome music in $directory"
