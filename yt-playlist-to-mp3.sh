5e66f53317659303e73d628efaff5aceafd7e667
ebiwoxite <77335575+ebiwoxite@users.noreply.github.com>
2024-02-24 12:51:02 +0100

    Update yt-playlist-to-mp3.sh
    Changed the way of applying metadata to the file, now using --embed-metadata yt-dlp option
    
    added the -t option to embed the thumbnail
    
    modified the way to build the yt-dlp command (now using eval) to facilitate future additions

--- yt-playlist-to-mp3.sh
+++ yt-playlist-to-mp3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-
 singlefile="false"
-while getopts ":d:su:" opt; do
+thumbnail="false"
+while getopts ":d:stu:" opt; do
     case $opt in
         d)
             directory="$OPTARG"
@@ -11,7 +11,10 @@ while getopts ":d:su:" opt; do
             singlefile="true"
             echo "Single file: $singlefile"
             ;;
-    
+        t)
+            thumbnail="true"
+            echo "Embed thumbnail: $thumbnail"
+            ;;
         u)
             url="$OPTARG"
             echo "Playlist url: $url"
@@ -37,34 +40,31 @@ if [ ! -d "$directory" ]; then
     exit 1
 fi
 
+command="yt-dlp -f 'ba' -x --audio-format mp3 --audio-quality 2 \"$url\" -o '%(uploader)sµ%(artist)sµ%(title)s.tmp' --embed-metadata --parse-metadata ':(?P<description>)' --windows-filenames --replace-in-metadata 'title' '^.*\s-\s|\s\[.*|\s\(.*|\s$' '' --replace-in-metadata 'uploader' '\s-\sTopic' ''"
+
 # Check if option 'singlefile' is provided
 if $singlefile; then
-    cd $directory
-    yt-dlp -f 'ba' -x --audio-format mp3 $url -o '%(title)s.mp3' --concat-playlist 'always'
-    exit 1
+    command="yt-dlp -f 'ba' -x --audio-format mp3 --recode-video mp3 --audio-quality 2 \"$url\" -o '%(title)s.mp3' --windows-filenames --concat-playlist 'always'"
+fi
+
+# Check if option 'thumbnail' is provided
+if $thumbnail; then
+    command="$command --embed-thumbnail"
 fi
 
 cd $directory
-
-yt-dlp -f 'ba' -x --audio-format mp3 $url -o '%(uploader)sµ%(artist)sµ%(title)s.tmp'
+eval $command
 
 for file in *.tmp*; do
-    #cleaning the name and applying tags (artist/title) to the file 
-    newfile=$(echo "$file" | sed 's/ - Topic//')
- 
-    #removes any part beween () or []
-    newfile=$(echo "$newfile" | sed 's/ *(.*)//') 
-    newfile=$(echo "$newfile" | sed 's/\[.*\]//')
-
-    basename="${newfile%%.tmp*}" #remove the extension (.tmp.mp3)
+    echo "Processing $file"
+    basename="${file%%.tmp*}" #remove the extension (.tmp.mp3)
     basename="${basename// - /µ}"
     IFS='µ'
     read -r uploader artist title <<< "$basename"
-
-    if [[ $title == *"µ"* ]]; then
-        read -r artist title <<< "$title"
-    fi
+    
     IFS=' '
+    
+    # the regex we did to clean the artist and title with yt-dlp does not apply to the name of the file so we need to clean it manually
     
     if [[ $artist == "NA" ]]; then
         echo 'artist is NA' 
@@ -73,26 +73,20 @@ for file in *.tmp*; do
         artist="${artist%% Official*}"
         artist="${artist%%VEVO*}"
     fi
-    #if there are featurings, it only take the first artist
+    #if there are featurings, it only take the first artist for the file name
     artist="${artist%%,*}" 
     artist="${artist%%feat*}"
 
-    #strip spaces from the end of the strings
-    artist=$(echo "$artist" | sed 's/[[:space:]]*$//')
-    title=$(echo "$title" | sed 's/[[:space:]]*$//')
-    if [ -n "$artist" ] && [ -n "$title" ]; then #to verify if they are null
-        echo "Applying tags..." 
-        kid3-cli -c "set artist \"$artist\"" -c "set title \"$title\"" "$file"
+    #verify if artist+title are null
+    if [ -n "$artist" ] && [ -n "$title" ]; then 
         newfile="$artist - $title.mp3"
     else
-        echo "problem while applying tags $file"
+    	newfile="$file.error.mp3"
+        echo "problem while identifying artist and title: $file"
     fi
-    
-    #removes any illegal characters for file names 
-    newfile=$(echo "$newfile" | tr -d ':?|!$#%&{}<>*/')   
+      
     mv "$file" "$newfile"
     echo "Renamed $newfile"
-    # [COMING NEXT] putting the thumbnail of the video
 done
 
 echo "Finished renaming and tagging files"
